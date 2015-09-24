72ca83011e5c8db8ebddc9515ec5e2d77cdf59b5
Ciaran Lofts <cjlofts@sky.com>
2015-09-23 17:12:06 -0700

    minor change

--- spec/eventbrite_contacts_spec.rb
+++ spec/eventbrite_contacts_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe EventbriteContacts do
   it 'has a version number' do
-    expect(EventbriteContacts::VERSION).not_to be nil
+    expect(EventbriteContacts::Client::VERSION).not_to be nil
   end
 
   it 'does something useful' do
