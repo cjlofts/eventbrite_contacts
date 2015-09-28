be48c7da74ec6d6a82b11b3c0d6cc38cf0f34b7c
Ciaran Lofts <cjlofts@sky.com>
2015-09-28 14:32:59 -0700

    raise exception if rate limit hit

--- lib/eventbrite_contacts.rb
+++ lib/eventbrite_contacts.rb
@@ -2,6 +2,7 @@ require 'eventbrite_contacts/version'
 require 'httparty'
 
 module EventbriteContacts
+  RateLimitExceeded = Class.new(StandardError)
   class Client
 
   BASE_URL = "https://www.eventbriteapi.com/v3/"
@@ -15,25 +16,29 @@ module EventbriteContacts
     url = "#{BASE_URL}users/#{@user_id}/contact_lists/?"
     page = if options[:page].nil? then 1 else options[:page] end
     encoded = URI.encode("#{url}page=#{page}")
-    make_request(encoded, "get")
+    response = make_request(encoded, "get")
+    raise_limit_error(response)
   end
 
   def get_list(list_id)
     url = "#{BASE_URL}users/#{@user_id}/contact_lists/#{list_id}"
     encoded = URI.encode(url)
-    make_request(encoded, "get")
+    response = make_request(encoded, "get")
+    raise_limit_error(response)
   end
 
   def add_list(list_name)
     url = "#{BASE_URL}users/#{@user_id}/contact_lists/?"
     encoded = URI.encode("#{url}contact_list.name=#{list_name}")
-    make_request(encoded, "post")
+    response = make_request(encoded, "post")
+    raise_limit_error(response)
   end
 
   def delete_list(list_id)
     url = "#{BASE_URL}users/#{@user_id}/contact_lists/#{list_id}"
     encoded = URI.encode(url)
-    make_request(encoded, "delete")
+    response = make_request(encoded, "delete")
+    raise_limit_error(response)
   end
 
   def get_all_contacts(list_id)
@@ -43,6 +48,7 @@ module EventbriteContacts
     loop do
       encoded = URI.encode("#{url}?page=#{page}")
       response = make_request(encoded, "get")
+      raise_limit_error(response)
       contacts << response["contacts"] unless response["contacts"].nil?
       page += 1
       break if response["error"] == "BAD_PAGE"
@@ -54,20 +60,23 @@ module EventbriteContacts
     url = "#{BASE_URL}users/#{@user_id}/contact_lists/#{list_id}/contacts?"
     page = if options[:page].nil? then 1 else options[:page] end
     encoded = URI.encode("#{url}page=#{page}")
-    make_request(encoded, "get")
+    response = make_request(encoded, "get")
+    raise_limit_error(response)
   end
 
   def add_contact(list_id, contact)
     url = "#{BASE_URL}users/#{@user_id}/contact_lists/#{list_id}/contacts/?"
     param_url = "contact.email=#{contact[:email]}&contact.first_name=#{contact[:first_name]}&contact.last_name=#{contact[:last_name]}"
     encoded = URI.encode("#{url}#{param_url}")
-    make_request(encoded, "post")
+    response = make_request(encoded, "post")
+    raise_limit_error(response)
   end
 
   def delete_contact(list_id, contact_email)
     url = "#{BASE_URL}users/#{@user_id}/contact_lists/#{list_id}/contacts/?"
     encoded = URI.encode("#{url}email=#{contact_email}")
-    make_request(encoded, "delete")
+    response = make_request(encoded, "delete")
+    raise_limit_error(response)
   end
 
   private
@@ -80,6 +89,14 @@ module EventbriteContacts
       HTTParty.post(url, :headers => { "Authorization" => "Bearer #{@auth_token}" })
     when "delete"
       HTTParty.delete(url, :headers => { "Authorization" => "Bearer #{@auth_token}" })
+    end
+  end
+
+  def raise_limit_error(response)
+    if response["error"] == "HIT_RATE_LIMIT"
+      raise RateLimitExceeded, "rate_limit_exceeded"
+    else
+      response
     end
   end
 
